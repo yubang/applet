f9dbd2592bb6c2449519caef4f7eb74adf9a1367
yubang <yubang93@gmail.com>
2017-04-08 01:50:01 +0800

    增加配置文件

--- index.py
+++ index.py
@@ -1,7 +1,11 @@
 # coding:UTF-8
 
 from applet import Applet, AppletConfig
+from json import loads
 
-config = AppletConfig("./demo/in", "./demo/out")
+with open('config.json') as fp:
+    c = loads(fp.read())
+config = AppletConfig(c['applet_dir_path'], c['build_dir_path'])
 app = Applet(config)
 app.build()
+print "打包完成，文件输出于：", c['build_dir_path']
